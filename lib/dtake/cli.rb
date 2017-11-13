25c43f5feab9e2affa70ccfde287832d610132a3
nehuyu <ne.huyu@gmail.com>
2017-11-13 19:20:49 +0900

    split command into two commands

--- lib/dtake/cli.rb
+++ lib/dtake/cli.rb
@@ -8,17 +8,19 @@ module Dtake
     default_command :main
 		
 		desc "dtake some.rb", "download" # コマンドの使用例と、概要
-		def main(argv)
-			if argv == '-ls'
-				list_url = "https://web.sfc.keio.ac.jp/~takefuji/list.html" 
-				doc = Nokogiri::HTML(open(list_url))
-				doc.inner_html.split("\n").each do |line|
-					puts line
-				end
-			else
-				command = "wget https://web.sfc.keio.ac.jp/~takefuji/" + argv
-				system(command)
+		def ls()
+			list_url = "https://web.sfc.keio.ac.jp/~takefuji/list.html" 
+			doc = Nokogiri::HTML(open(list_url))
+			doc.inner_html.split("\n").each do |line|
+				puts line
 			end
 		end
+		
+		def d(filename)
+			command = "wget https://web.sfc.keio.ac.jp/~takefuji/" + filename
+			system(command)
+		end
+
 	end
 end
+
